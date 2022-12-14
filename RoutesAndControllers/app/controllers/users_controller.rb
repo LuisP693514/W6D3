5a31052231b3390e8fb71606c1d2e0ecfc21a836
Jiong Qi <jiongqipan@gmail.com>
2022-12-14 16:21:29 -0500

    Complete

--- RoutesAndControllers/app/controllers/users_controller.rb
+++ RoutesAndControllers/app/controllers/users_controller.rb
@@ -1,13 +1,47 @@
 class UsersController < ApplicationController
     def index 
-        render plain: "I'm in the index action!"
+        # render plain: "I'm in the index action!"
+        @users = User.all 
+        render json: @users
     end
 
     def create
-        render json: params
+        @user = User.new(userparam)
+        # user.save!
+        # render json: user
+
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+
+        render json: @user
+    end
+
+    def update 
+        @user = User.find(params[:id])
+
+        if @user.update(userparam)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to user_url
+    end
+    
+    private
+
+    def userparam
+        params.require(:users).permit(:name, :email)
     end
 end
