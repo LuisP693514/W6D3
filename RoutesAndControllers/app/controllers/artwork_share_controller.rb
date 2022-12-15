99a90d9597e37cfd5cf55882060271bfb59120a6
Jiong Qi <jiongqipan@gmail.com>
2022-12-15 12:09:24 -0500

    Complete Artwork_share

---
+++ RoutesAndControllers/app/controllers/artwork_share_controller.rb
@@ -0,0 +1,29 @@
+class ArtworkShareController < ApplicationController
+    def index
+        @artwork_shares = ArtworkShare.all
+        render json: @artwork_shares 
+    end
+
+    def create
+        @artwork_shares = ArtworkShare.new(artwork_shares_param)
+
+        if @artwork_shares.save
+            render json: @artwork_shares
+        else
+            render json: @artwork_shares.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @artwork_share = ArtworkShare.find(params[:id])
+        @artwork_share.destroy
+        @artwork_shares = ArtworkShare.all
+        render json: @artwork_shares
+    end
+
+    private
+
+    def artwork_shares_param
+        params.require(:artwork_shares).permit(:viewer_id, :artwork_id)
+    end
+end
--- RoutesAndControllers/db/seeds.rb
+++ RoutesAndControllers/db/seeds.rb
@@ -6,13 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(username: "bob1")
-User.create(username: "bob2")
+User.create(username: "bob2") 
 User.create(username: "bob3")
+User.create(username: "bob1") 
 User.create(username: "bob4")
 
 Artwork.create(title: "vinci", image_url: "url.com", artist_id: 1)
-Artwork.create(title: "vinci", image_url: "url4.com", artist_id: 1)
-Artwork.create(title: "da vii", image_url: "url3.com", artist_id: 1)
-Artwork.create(title: "dance", image_url: "ur2.com", artist_id: 1)
+Artwork.create(title: "vinci", image_url: "url4.com", artist_id: 3)
+Artwork.create(title: "da vii", image_url: "url3.com", artist_id: 2)
+Artwork.create(title: "dance", image_url: "ur2.com", artist_id: 2)
 
+ArtworkShare.create(viewer_id: 3, artwork_id: 3)
